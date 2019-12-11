80b2b60af8a348ce593bd082d977834134a6a0d6
Cristina Rota <cristina.rota.m@gmail.com>
2019-12-11 20:27:55 +0100

    testing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 
-from src.data import getData
+import pandas as pd
+from src.data import *
 
-# Get data from the API:
-DF = getData('spain')
+# Get data:
 
+# DF = getData('spain')
+DF = pd.read_csv('./dataset/birds_spain.csv')
+
+# speciesArray = ['European Robin', 'Iberian Green Woodpecker', 'Little Egret', 'Northern Raven']
+# DF_selected = selectSpecies(speciesArray, DF)
+DF_selected = pd.read_csv('./dataset/birds_spain_selected.csv')
+
+
+# from pydub import AudioSegment
+# audio = AudioSegment.from_mp3('./dataset/recordings/stage-1/Erithacus-rubecula_389544.mp3')
--- src/data.py
+++ src/data.py
@@ -1,8 +1,12 @@
 
+import pandas as pd
 from urllib.request import urlretrieve
 import requests
 import json
-import pandas as pd
+import shutil
+from os import listdir
+from os.path import isfile, join
+import re
 
 
 def getData(country):
@@ -17,7 +21,7 @@ def getData(country):
     res = requests.get(url)
     res_dict = json.loads(res.text)
     df = pd.DataFrame()
-    # errors = []
+    errors = []
     for i in range(1,res_dict['numPages']+1):
         url_page = f'https://www.xeno-canto.org/api/2/recordings?query=cnt:{country}&page={i}'
         res_page = requests.get(url_page)
@@ -32,12 +36,44 @@ def getData(country):
                 urlretrieve(url,
                 f"./dataset/recordings/{res_dict_page['recordings'][j]['gen']}-{res_dict_page['recordings'][j]['sp']}_{id}.mp3")
             except:
-                # errors.append(id)
+                errors.append(id)
                 print(f'Download failed for id: {id}')
                 continue
-    # Reordering the columns of the dataframe:
+    # Drop recordings that failed to download from the dataframe:
+    indexsToDrop = []
+    for e in errors:
+        for i in range(len(df)):
+            if df.at[i,'id']== int(e):
+                indexsToDrop.append(i)
+    df.drop(indexsToDrop, axis=0, inplace=True)
+    # Reordering columns:
     df = df[['id', 'en','gen', 'sp', 'ssp', 'loc', 'cnt', 'lat', 'lng', 'alt', 'date', 'time',
     'also', 'bird-seen', 'type', 'url', 'file', 'file-name','length', 'lic','playback-used',
     'q','rec', 'rmk', 'sono', 'uploaded']]
     df.to_csv(f'./dataset/birds_{country}.csv', index=False)
     return df
+
+
+
+def selectSpecies(speciesArray, DF):
+    '''
+    Extract the selected species from the dataframe and save the requested audios in a separate folder.
+    Returns the cleaned dataframe.
+    '''
+    # For the moment, I remove the rows in which it is recorded that different species sing at the same time.
+    DFunique = DF[DF.also == "['']"]
+    # Getting ids for the selected species:
+    selectedIds = []
+    for e in speciesArray:
+        selectedIds.append(list(DFunique.id[DFunique.en==e]))
+    selectedIds = [item for sublist in selectedIds for item in sublist]
+    mypath = './dataset/recordings'
+    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    for file in files:
+        if int(re.findall('[0-9]+.',file)[0][:-1]) in selectedIds:
+            shutil.copy(f'./dataset/recordings/{file}', './dataset/recordings/stage-1')
+    indexsSelected = [i for i in range(len(DFunique)) if DFunique['id'].iloc[i] in selectedIds]
+    finalDF = DFunique.iloc[indexsSelected]
+    finalDF.to_csv(f'./dataset/birds_spain_selected.csv', index=False)
+    return finalDF
+
