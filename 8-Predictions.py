aaa07d41957f943ee0c02d7c49d3a46c7bc9c166
Cristina Rota <cristina.rota.m@gmail.com>
2019-12-16 12:25:53 +0100

    Predictions

--- 8-Predictions.py
+++ 8-Predictions.py
@@ -3,20 +3,20 @@ import subprocess
 from src.audioProcessing import *
 from keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
+from collections import Counter
+
+# I will receive a filename from the application:
+filename = 'Cc.mp3'
 
 # Audio processing:
 subprocess.run(['sh','./src/testAudioProcessing.sh'])
-featuresDf = featuresPipeline('./dataset/test/converted/lp_ng', 'test')
-# featuresDf = pd.read_pickle('./dataset/featuresDF_test_LN.pkl')
-
-# Tengo que hacer otra función para el test que recoja sólo X.
-# Tiene que guardar también una ID (nombre del fichero) para luego poder hacer la media
-# de las ventanas del audio.
+testFeaturesDf = testFeaturesPipeline('./dataset/test/converted/lp_ng', filename)
+# testFeaturesDf = pd.read_pickle('./dataset/featuresDF_test_LN.pkl')
 
 # Preparing data:
-featuresDf['fourier_mfcc'] = [np.concatenate([featuresDf.fourier[i],
-                              featuresDf.mfcc[i]]) for i in range(len(featuresDf))]
-X = np.array(featuresDf['fourier_mfcc'].tolist())
+testFeaturesDf['fourier_mfcc'] = [np.concatenate([testFeaturesDf.fourier[i],
+                              testFeaturesDf.mfcc[i]]) for i in range(len(testFeaturesDf))]
+X = np.array(testFeaturesDf['fourier_mfcc'].tolist())
 X = X.reshape(-1, 12, 32, 1)
 
 # Load trained model:
@@ -29,20 +29,22 @@ ynew = loaded_model.predict_classes(X)
 # the inverse_transform() function.
 encoder = LabelEncoder()
 encoder.classes_ = np.load('./models/classes.npy')
-encoder.inverse_transform([0])
 
-encoder.classes_
-# ['Corvus-corax', 'Egretta-garzetta', 'Erithacus-rubecula','Picus-sharpei']
+# encoder.classes_
 # 0: Corvus-corax
 # 1: Egretta-garzetta
 # 2: Erithacus-rubecula
 # 3: Picus-sharpei
 
+# Counting how many times each species appears in the predictions:
+windowPredictions = Counter(ynew)
+# Returning the species that appears more times:
+max_key = max(windowPredictions, key=lambda x: windowPredictions[x])
+finalPrediction = encoder.inverse_transform([max_key])[0]
+
 # Probability Predictions:
 # Predicts the probability of the data instance  belonging to each class.
-ynew = loaded_model.predict_proba(X) # ynew=loaded_model.predict(X)
-
+# ynew = loaded_model.predict_proba(X) # ynew=loaded_model.predict(X)
 # The output of our network is 4 probabilities (because of softmax),
 # so we can use np.argmax() to turn those into actual classes.
-print(np.argmax(ynew, axis=1))
-
+# print(np.argmax(ynew, axis=1))
--- src/audioProcessing.py
+++ src/audioProcessing.py
@@ -100,3 +100,48 @@ def featuresPipeline(filespath, stage):
     # return DFBalanced
     return DF
 
+
+def testFeaturesPipeline(filespath, filename):
+    '''
+    For new file (test):
+    - Separates out silent chunks.
+    - Splits each remaining chunk into 1 second windows overlapping by 50%.
+    - Stores the windows' array in a dataframe.
+    - Calculates Fourier coefficients for each window and stores them in the dataframe.
+    - Calculates Mels coefficients for each window and stores them in the dataframe.
+    - Returns the final dataframe.
+    '''
+    windowsList = []
+    files = [f for f in listdir(filespath) if isfile(join(filespath, f))]
+    for file in files:
+        if file == filename:
+            # Get the track and split it where the silence is 0.1 seconds or more.
+            chunks = splitOnSilence(f'{filespath}/{file}')
+            # print(f'Splitting {file}')        
+            for chunk in chunks:
+                # Define windows with overlap:
+                windowLen = 1000
+                overlap = 500
+                if len(chunk) >= 1000:
+                    for i in range(0,len(chunk),overlap):
+                        if i+windowLen <= len(chunk):
+                            window = chunk[i:i+windowLen]
+                            # window = window.set_frame_rate(48000) # I can change array's length with this (48000 for convention)
+                            # Get array from each window:
+                            sample = window.get_array_of_samples()
+                            # sample_np = np.array(sample.tolist(), dtype=np.float64)
+                            # Check if window has at least a determined amplitude:
+                            if np.max(sample) > 1500:
+                                # Fourier:
+                                fft_mod = fourierCoefficients(sample)
+                                # MFCC:
+                                mels = mfccCoefficients(sample)
+                                # Array of dictionaries:
+                                # windowsList.append({'class':species,'id':fileID,'sound':sample_np,'fourier':fft_mod,'mfcc':mels})
+                                windowsList.append({'fourier':fft_mod,'mfcc':mels})
+        DF = pd.DataFrame(windowsList)
+        # DF = DF[['class','id','sound','fourier','mfcc']]
+        DF = DF[['fourier','mfcc']]
+        # DF['fourier_mfcc'] = [np.concatenate([DF.fourier[i], DF.mfcc[i]]) for i in range(len(DF))]
+        DF.to_pickle(f'./dataset/featuresDF_test_LN.pkl')
+        return DF
