17b4d372e718bd160fbd45afa48e226c77e50323
Cristina Rota <cristina.rota.m@gmail.com>
2019-12-17 16:57:21 +0100

    Refactoring

--- 1-gettingData.py
+++ 1-gettingData.py
@@ -6,17 +6,15 @@ from src.getData import *
 df = getData('spain')
 # df = pd.read_csv('./dataset/birds_spain.csv')
 
-
-# Stage 1:
+# Stage 1: 4 species
 speciesArray = ['European Robin', 'Iberian Green Woodpecker', 'Little Egret', 'Northern Raven']
 df_selected = selectSpecies(speciesArray, df, 1)
 # df_selected = pd.read_csv('./dataset/birds_spain_selected_1.csv')
 
 
-# Stage 2:
+# Stage 2: 10 species
 speciesArray = ['European Robin', 'Common Cuckoo', 'European Greenfinch', 'Black-winged Kite',
                 'European Serin', 'Iberian Green Woodpecker', 'Little Egret', 'Little Owl',
                 'Northern Raven']
 df_selected = selectSpecies(speciesArray, df, 2)
 # df_selected = pd.read_csv('./dataset/birds_spain_selected_2.csv')
-
--- application/start.py
+++ application/start.py
@@ -12,7 +12,7 @@ def index():
 def Birdentifier():
 	filename = request.form['filename']
 	if filename:
-		subprocess.run(['python', './8-Predictions.py', filename])
+		subprocess.run(['python', './5-Predictions.py', filename])
 		return 'The answer to your request will be opened in a new browser tab. Thank you for using our service.'
 	else:
 		return 'No file chosen. Please go back and select a file before pressing the submit button.'
