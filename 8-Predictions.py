7e7dc3ee857fbff95aa050709f9c34ecabfd0c2a
Cristina Rota <cristina.rota.m@gmail.com>
2019-12-17 13:55:50 +0100

    Predictions

--- 8-Predictions.py
+++ 8-Predictions.py
@@ -15,7 +15,7 @@ filename = sys.argv[1]
 # Audio processing:
 subprocess.run(['sh','./src/testAudioProcessing.sh', filename])
 testFeaturesDf = testFeaturesPipeline('./dataset/test/converted/lp_ng', filename)
-# testFeaturesDf = pd.read_pickle('./dataset/featuresDF_test_LN.pkl')
+## testFeaturesDf = pd.read_pickle('./dataset/featuresDF_test_LN.pkl')
 
 # Preparing data:
 testFeaturesDf['fourier_mfcc'] = [np.concatenate([testFeaturesDf.fourier[i],
--- src/testAudioProcessing.sh
+++ src/testAudioProcessing.sh
@@ -1,12 +1,10 @@
 
 filename=${1}
-echo $filename
 
 # lowpass + noise gate:
 for input in $(ls './dataset/test' | egrep '\.mp3$')
    do
-      if [ $input == $filename ]; then       
-         echo $input
+      if [ $input == $filename ]; then
          sox "./dataset/test/$input" "./dataset/test/converted/lp/$input" lowpass 3000
          echo "lowpass applied in $input"
       fi
